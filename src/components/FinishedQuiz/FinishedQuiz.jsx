53553294a8041a0c3a312e876655b92507ad62d1
Daniel <KunitskiiDanik@mail.ru>
2022-03-28 22:30:22 +0300

    refactoring the finishedQuiz

--- src/components/FinishedQuiz/FinishedQuiz.jsx
+++ src/components/FinishedQuiz/FinishedQuiz.jsx
@@ -12,25 +12,28 @@ const FinishedQuiz = (props) => {
         return total
     }, 0), [props.results])
 
+    //eslint-disable-next-line
+    const successList = useCallback(props.quiz.map((quizItem, index) => {   
+        const cls = [
+            'fa',
+            props.results[quizItem.id] === 'error' ? 'fa-times' : 'fa-check',
+            classes[props.results[quizItem.id]]
+        ]    
+        return (
+            <li 
+                key={index}
+            >
+                <strong>{index + 1}</strong>.&nbsp;
+                {quizItem.question}
+                <i className={cls.join(' ')} />
+            </li>
+        )
+    }), [props.quiz])
+
     return (
         <div className={classes.FinishedQuiz}>
             <ul>
-                {props.quiz.map((quizItem, index) => {   
-                    const cls = [
-                        'fa',
-                        props.results[quizItem.id] === 'error' ? 'fa-times' : 'fa-check',
-                        classes[props.results[quizItem.id]]
-                    ]    
-                    return (
-                        <li 
-                            key={index}
-                        >
-                            <strong>{index + 1}</strong>.&nbsp;
-                            {quizItem.question}
-                            <i className={cls.join(' ')} />
-                        </li>
-                    )
-                })}
+                {successList}
             </ul>
 
             <p>Правильно {successCount} из {props.quiz.length}</p>
