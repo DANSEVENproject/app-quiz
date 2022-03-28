0521483e7334def6b7145662bd922167e4adc3db
Daniel <KunitskiiDanik@mail.ru>
2022-03-28 22:34:41 +0300

    refactoring the ActiveQuiz

--- src/components/ActiveQuiz/AnswersList/AnswersList.jsx
+++ src/components/ActiveQuiz/AnswersList/AnswersList.jsx
@@ -1,19 +1,26 @@
 import classes from './AnswersList.module.css'
 import AnswerItem from './AnswerItem/AnswerItem'
+import { useCallback } from 'react'
 
-const AnswersList = (props) => (
-    <ul className={classes.AnswersList}>
-        { props.answers.map((answer, index) => {
-            return (
-                <AnswerItem 
-                    key={index}
-                    answer={answer}
-                    onAnswerClick={props.onAnswerClick}
-                    state={props.state ? props.state[answer.id] : null}
-                />
-            )
-        }) }
-    </ul>
-)
+const AnswersList = (props) => {
+    //eslint-disable-next-line
+    const answersMap = useCallback(props.answers.map((answer, index) => {
+        return (
+            <AnswerItem 
+                key={index}
+                answer={answer}
+                onAnswerClick={props.onAnswerClick}
+                state={props.state ? props.state[answer.id] : null}
+            />
+        )
+    }), [props.answers])
+
+    return (
+        <ul className={classes.AnswersList}>
+                {answersMap}
+        </ul>
+    )
+    
+}
 
 export default AnswersList
